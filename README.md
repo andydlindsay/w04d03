290d7c2301be311c6b788075c477864485a7a092
Andy Lindsay <andydlindsay@gmail.com>
2019-07-17 12:10:44 -0700

    add ajax post request to create new posts

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # W04D03 AJAX
 
 ### To Do
-- [ ] Use AJAX to retrieve data from a remote server
-- [ ] Use jQuery to update the DOM with the retrieved data
-- [ ] Use AJAX to submit data to a remote server
-- [ ] All without refreshing the browser
+- [x] Use AJAX to retrieve data from a remote server
+- [x] Use jQuery to update the DOM with the retrieved data
+- [x] Use AJAX to submit data to a remote server
+- [x] All without refreshing the browser
 
 ## AJAX
 
--- public/app.js
+++ public/app.js
@@ -2,18 +2,43 @@ $(() => {
 
   const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-  $('#get-posts').click(() => {
-    $.getJSON(`${BASE_URL}/posts`, (posts) => {
-      posts.slice(0, 10).forEach((post) => {
-        const $posts = $('#posts');
-        const $article = $('<article>');
-        const $title = $('<header>').text(`Title: ${post.title}`);
-        const $body = $('<p>').text(post.body);
-        const $footer = $('<footer>').text(`user: ${post.userId}`);
-        $article.append($title, $body, $footer);
-        $posts.append($article);
-      });
+  $('#new-post').on('submit', (event) => {
+    event.preventDefault();
+    $.post(`${BASE_URL}/posts`, $('#new-post').serialize(), (post) => {
+      appendPost(post, $('#posts'));
     });
+  });
+
+  const appendPost = (post, $posts) => {
+    
+    const $article = $('<article>');
+    const $title = $('<header>').text(`Title: ${post.title} (${post.id})`);
+    const $body = $('<p>').text(post.body);
+
+    $.getJSON(`${BASE_URL}/users/${post.userId}`, (user) => {
+      const $footer = $('<footer>').text(`User: ${user.name} (${user.email})`);
+      $article.append($title, $body, $footer);
+      $posts.append($article);
+    });
+  };
+
+  $('#get-posts').click(() => {
+    const $posts = $('#posts');
+    $posts.empty();
+    // $.getJSON(`${BASE_URL}/posts`, (posts) => {
+    //   posts.slice(5, 15).forEach((post) => {
+    //     appendPost(post, $posts);
+    //   });
+    // });
+    $.getJSON(`${BASE_URL}/posts`)
+      .done((posts) => {
+        posts.slice(5, 15).forEach((post) => {
+          appendPost(post, $posts);
+        });
+      })
+      .fail((error) => {
+        console.error(error);
+      });
   });
 
 });
